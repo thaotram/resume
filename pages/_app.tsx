6b883863dc750946b33b0d42449ce1a08f28ee3c
thaotram <sawa.kisei@gmail.com>
2021-01-26 22:01:35 +0700

    feat(tailwindcss): add `tailwindcss`

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import type { AppProps } from 'next/app';
+import '../styles/index.scss';
+
+export default function App({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    purge: ['./pages/**/*.{jsx,tsx}', './components/**/*.{jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {},
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
