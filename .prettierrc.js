38e3c4c31e58411c041fc9ffef2f745d8b3a89b2
thaotram <sawa.kisei@gmail.com>
2021-01-27 10:03:47 +0700

    chore(stylelint): add `stylelint` & plugins

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,26 @@
 module.exports = {
     ...require('@culur/prettier-config'),
+    overrides: [
+        {
+            files: [
+                // alphabetical order
+                '.babelrc',
+                '.czrc',
+                '.releaserc',
+                '.stylelintrc',
+                '*.code-workspace',
+            ],
+            options: { parser: 'json' },
+        },
+        {
+            files: [
+                // alphabetical order
+                'package.json',
+                'package-lock.json',
+                '*.md',
+                '*.yml',
+            ],
+            options: { tabWidth: 2 },
+        },
+    ],
 };
