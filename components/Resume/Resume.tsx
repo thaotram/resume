7f7ca08c3a4051582c466879065a44f66344ca96
thaotram <sawa.kisei@gmail.com>
2021-02-06 15:47:03 +0700

    feat(layout): add `ResumeName`

--- components/Resume/Resume.tsx
+++ components/Resume/Resume.tsx
@@ -1,7 +1,9 @@
+import ResumeName from './ResumeName';
 export default function Resume() {
     return (
         <div className="container">
             <div className="bg-white shadow-md">
+                <ResumeName />
             </div>
         </div>
     );
---
+++ components/Resume/ResumeName.tsx
@@ -0,0 +1,14 @@
+export default function ResumeName() {
+    return (
+        <div className="font-display relative pr-12 pt-20 text-right">
+            <div className="tracking-title text-lg font-medium">
+                LE THI THAO
+            </div>
+            <div className="-mt-1 text-4xl font-bold tracking-widest">TRAM</div>
+            <div className="mt-2 px-3 py-2 text-xs font-medium tracking-widest bg-yellow-300 uppercase">
+                Front-end Web Developer
+            </div>
+            <div className="absolute bottom-3 top-0 ml-17 w-px bg-black"></div>
+        </div>
+    );
+}
