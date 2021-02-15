3c4cfe548a701ef9591a81dec5ea4ed4f3ed5ff7
thaotram <sawa.kisei@gmail.com>
2021-02-15 11:29:06 +0700

    feat(layout): update `ResumeName`

--- components/Resume/ResumeName.tsx
+++ components/Resume/ResumeName.tsx
@@ -1,14 +1,23 @@
+import GridContentWrapper from '../Utils/GridContentWrapper';
+import GridContent from '../Utils/GridContent';
+import styles from './ResumeName.module.scss';
+
 export default function ResumeName() {
     return (
-        <div className="font-display relative pr-12 pt-20 text-right">
-            <div className="tracking-title text-lg font-medium">
-                LE THI THAO
-            </div>
-            <div className="-mt-1 text-4xl font-bold tracking-widest">TRAM</div>
-            <div className="mt-2 px-3 py-2 text-xs font-medium tracking-widest bg-yellow-300 uppercase">
+        <div className="pt-14 text-right">
+            <GridContentWrapper className={styles.name}>
+                <GridContent>
+                    <div className="text-lg font-medium tracking-title -mr-title">
+                        LE THI THAO
+                    </div>
+                    <div className="-mt-1 text-4xl font-bold tracking-widest -mr-widest">
+                        TRAM
+                    </div>
+                </GridContent>
+            </GridContentWrapper>
+            <div className="mr-12 px-3 py-2 text-xs font-medium tracking-widest bg-yellow-300 uppercase">
                 Front-end Web Developer
             </div>
-            <div className="absolute bottom-3 top-0 ml-17 w-px bg-black"></div>
         </div>
     );
 }
